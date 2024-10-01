72c8601408ffb6a09cb43a06a3d5f27ab8996cdc
Magnus <magn4496@stud.kea.dk>
2024-10-01 09:49:30 +0200

    samlet backend og frontend

--- rawg-server/routers/genreMysqlRouters.ts
+++ rawg-server/routers/genreMysqlRouters.ts
@@ -1,1 +1,33 @@
+import { Router } from 'express';
+import { AppDataSource } from '../startup/data-source';
+import { Genres } from '../entities/Genres';
 
+interface Response {
+    count: number;
+    results: Genres[];
+}
+
+const router = Router();
+const genresRepository = AppDataSource.getRepository(Genres);
+
+// Get all genres
+router.get('/', async (req, res) => {
+  const genres = await genresRepository.find();
+  const response: Response = {
+    count: genres.length,
+    results: genres,
+  };
+  res.json(response);
+});
+
+// Get a single genre by ID
+router.get('/:id', async (req, res) => {
+  const genre = await genresRepository.findOne({ where: { id: Number(req.params.id) } });
+  if (genre) {
+    res.json(genre);
+  } else {
+    res.status(404).json({ message: 'Genre not found' });
+  }
+});
+
+export default router;
--- rawg-server/startup/setupMysqlRouters.ts
+++ rawg-server/startup/setupMysqlRouters.ts
@@ -4,6 +4,7 @@ import gameMysqlRouters  from '../routers/gameMysqlRouters';
 
 const setupMysqlRouters = (app: express.Application) => {
     app.use("/games", gameMysqlRouters);
+    // app.use("/genres", genreRouters);
 }
 
 export default setupMysqlRouters;
